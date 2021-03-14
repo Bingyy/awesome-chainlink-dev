364f0897b1ef848b1cdb74d5fadc4dc732ffebb6
Bingyy <bing@bingw.me>
2021-03-14 12:08:48 +0800

    add ExternalAdapterTemplate

---
+++ ExternalAdapterTutorials/ExternalAdapterTemplate/app.js
@@ -0,0 +1,25 @@
+const createRequest = require('./index').createRequest
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.EA_PORT || 8080
+
+app.use(bodyParser.json())
+
+// 测试GET请求
+app.get('/', (req, res) => {
+  console.log("Hello!")
+  res.send("<h1>Welcome!</h1>")
+})
+
+// POST请求
+app.post('/', (req, res) => {
+  console.log('POST Data: ', req.body)
+  createRequest(req.body, (status, result) => {
+    console.log('Result: ', result)
+    res.status(status).json(result)
+  })
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
